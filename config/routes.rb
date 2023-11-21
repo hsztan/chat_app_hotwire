6a2df71dd6e4e5488b0c237a024290421d208877
Henry Nawrocki Sztanski <henry@sztanski.com>
2023-11-20 22:21:30 -0500

    Add Room model, RoomsHelper module, and room views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "posts/index"
-  root "posts#index"
+  root "rooms#index"
+
+  resources :rooms
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
